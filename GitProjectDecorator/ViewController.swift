4d2d29ca5b0d8e2cfa72402cec31712138f6232f
Olha Pavliuk <olia.pavliuk@gmail.com>
2021-09-12 17:10:02 +0300

    Don't know how to "Embed in Navigation Controller" programmatically. So let's assume this is not needed now for "file swizzling".

--- GitProjectDecorator/ViewController.swift
+++ GitProjectDecorator/ViewController.swift
@@ -1,14 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tapGestureRecognizer: UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         UIView.animate(withDuration: 5) {
             self.view.backgroundColor = .blue
         }
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onSingleTapped))
+        view.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
+    @objc func onSingleTapped() {
+        let vc2 = ViewController2()
+        if navigationController == nil {
+            UIApplication.shared.windows.first!.rootViewController = UINavigationController(rootViewController: self)
+        }
+        self.navigationController?.pushViewController(vc2, animated: true)
+    }
 
 }
 
